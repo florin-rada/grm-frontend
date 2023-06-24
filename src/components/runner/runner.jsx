23c0339be9f4716591f0282438b2fb4cdb0bda82
Rada Florin Daniel <florin.rada87@yahoo.com>
2023-06-25 00:09:09 +0300

    Added HeadlessUI to dependencies Used HeadlessUI Modal for the details.

--- src/components/runner/runner.jsx
+++ src/components/runner/runner.jsx
@@ -1,7 +1,7 @@
 import { faGear, faCircleCheck, faXmark, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { Modal } from "../modal/modal";
+import { Dialog } from "@headlessui/react";
 
 export const Runner = ({name}) => {
     const [showDetails, setShowDetails] = useState(false);
@@ -15,11 +15,11 @@ export const Runner = ({name}) => {
     }
 
     return (
-        <div className="flex flex-col rounded-lg min-w-min border border-slate-500 dark:border-slate-200">
+        <div className="flex flex-col rounded-lg min-w-min border border-slate-500 dark:border-slate-200 z-0">
             <div onClick={onCardClick} className="flex header border-b border-b-slate-400 p-2 justify-center hover:cursor-pointer">
                 <span>{name}</span>
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col z-0">
                 <div className="px-1 flex flex-row justify-start align-text-top p-1">
                     <div>
                         <FontAwesomeIcon color="blue" spin icon={faGear}></FontAwesomeIcon>
@@ -61,7 +61,25 @@ export const Runner = ({name}) => {
                     </div>
                 </div>
             </div>
-            <Modal show={showDetails} handleOnClose={closeRunnerDetails}></Modal>
+            <Dialog open={showDetails} onClose={closeRunnerDetails} className="relative z-50">
+                <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+                <div className="fixed inset-0 flex items-center justify-center p-4">
+                    <Dialog.Panel>
+                        <Dialog.Title>Deactivate account</Dialog.Title>
+                        <Dialog.Description>
+                        This will permanently deactivate your account
+                        </Dialog.Description>
+
+                        <p>
+                        Are you sure you want to deactivate your account? All of your data
+                        will be permanently removed. This action cannot be undone.
+                        </p>
+
+                        <button onClick={() => closeRunnerDetails()}>Deactivate</button>
+                        <button onClick={() => closeRunnerDetails()}>Cancel</button>
+                    </Dialog.Panel>
+                </div>
+            </Dialog>
         </div>
     )
 }
